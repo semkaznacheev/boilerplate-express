3a9beeef27aaa27759187c6609a8a27f02b10743
Semen Kaznacheev <semkaznacheev@gmail.com>
2023-06-05 18:57:00 +0100

    d

--- myApp.js
+++ myApp.js
@@ -6,6 +6,9 @@ res.sendFile(__dirname + "/views/index.html")
 })
 app.use('/public', express.static(__dirname + "/public"))
 
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"})
+})
 
 
 
